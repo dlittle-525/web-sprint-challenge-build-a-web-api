e945d59ced3729f5150d6f8317ffe03fefa7313f
Douglas Little <douglas.a.little@gmail.com>
2021-04-16 20:45:36 -0400

    Index and srever

--- api/server.js
+++ api/server.js
@@ -3,5 +3,17 @@ const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+const actionRouter = require("./actions/actions-router")
+const projectRouter = require("./projects/projects-router")
+
+server.use(actionRouter)
+server.use(projectRouter)
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: "Something went wrong, please try again later",
+    })
+})
 
 module.exports = server;
